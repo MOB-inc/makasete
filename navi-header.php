7ceedfd2c00a5ee0e79de353c74c8605560cf1e7
hirofumi-tokunaga <mobinc.tokunaga@gmail.com>
2022-12-23 14:16:50 +0900

    head

--- navi-header.php
+++ navi-header.php
@@ -1,22 +1,25 @@
 <header id="header">
-	<nav class="nav">
-		<ul class="nav__wrapper flex">
-			<li><a href="<?php echo esc_url(home_url()); ?>/#service">対応サービス</a></li>
-			<li><a href="<?php echo esc_url(home_url()); ?>/#delivery">ご利用の流れ</a></li>
-			<li><a href="<?php echo esc_url(home_url()); ?>/#contract">プラン</a></li>
-			<li><a href="<?php echo esc_url(home_url()); ?>/#faq">FAQ</a></li>
-			<li class="contact"><a href="<?php echo esc_url(home_url()); ?>/#contact">お問い合わせ</a></li>
-			<li><a href="<?php echo esc_url(home_url()); ?>/application">お申し込み</a></li>
-		</ul>
-	</nav>
-	<!-- ハンバーガーメニュー -->
-	<div class="hamburger" id="hamburger">
-		<span></span>
-		<span></span>
-		<span></span>
-	</div>
-	<nav class="globalMenuSp">
+	<div class="container-m">
+		<h1 class="logo">MAKASETE</h1>
+		<nav class="pc">
+			<ul class="flex">
+				<li><a href="<?php echo esc_url(home_url()); ?>/#service">対応サービス</a></li>
+				<li><a href="<?php echo esc_url(home_url()); ?>/#delivery">ご利用の流れ</a></li>
+				<li><a href="<?php echo esc_url(home_url()); ?>/#contract">プラン</a></li>
+				<li><a href="<?php echo esc_url(home_url()); ?>/#faq">FAQ</a></li>
+				<li class="contact"><a href="<?php echo esc_url(home_url()); ?>/#contact" class="flex"><img src="<?php echo esc_url(get_theme_file_uri()); ?>/img/ico_contact.png" width="21" height="21" alt="">お問い合わせ</a></li>
+				<li class="application"><a href="<?php echo esc_url(home_url()); ?>/application" class="flex"><img src="<?php echo esc_url(get_theme_file_uri()); ?>/img/ico_application.png" width="21" height="21" alt="">お申し込み</a></li>
+			</ul>
+		</nav>
+		<!-- ハンバーガーメニュー -->
+		<div id="drwBtn" class="sp">
+			<span></span>
+			<span></span>
+			<span></span>
+		</div>
+		<nav id="drwMenu" class="sp">
 
-	</nav>
+		</nav>
+	</div>
 
 </header>
