3c4113428ec8c499b2603a6165e20aeb67eb33d5
hirofumi-tokunaga <mobinc.tokunaga@gmail.com>
2022-12-23 15:29:32 +0900

    hum

--- navi-footer.php
+++ navi-footer.php
@@ -2,7 +2,7 @@
 	<div class="container-m">
 		<nav>
 			<ul>
-				<li><a href="">会社概要</a></li>
+				<li><a href="https://loot.co.jp/" target="_blank" rel="noopener noreferrer">会社概要</a></li>
 				<li><a href="">個人情報保護方針</a></li>
 				<li><a href="">利用規約</a></li>
 			</ul>
--- navi-header.php
+++ navi-header.php
@@ -3,12 +3,11 @@
 		<h1 class="logo">MAKASETE</h1>
 		<nav class="pc">
 			<ul class="flex">
-				<li><a href="<?php echo esc_url(home_url()); ?>/#service">対応サービス</a></li>
 				<li><a href="<?php echo esc_url(home_url()); ?>/#delivery">ご利用の流れ</a></li>
 				<li><a href="<?php echo esc_url(home_url()); ?>/#contract">プラン</a></li>
 				<li><a href="<?php echo esc_url(home_url()); ?>/#faq">FAQ</a></li>
 				<li class="contact"><a href="<?php echo esc_url(home_url()); ?>/#contact" class="flex"><img src="<?php echo esc_url(get_theme_file_uri()); ?>/img/ico_contact.png" width="21" height="21" alt="">お問い合わせ</a></li>
-				<li class="application"><a href="<?php echo esc_url(home_url()); ?>/application" class="flex"><img src="<?php echo esc_url(get_theme_file_uri()); ?>/img/ico_application.png" width="21" height="21" alt="">お申し込み</a></li>
+
 			</ul>
 		</nav>
 		<!-- ハンバーガーメニュー -->
@@ -17,9 +16,15 @@
 			<span></span>
 			<span></span>
 		</div>
-		<nav id="drwMenu" class="sp">
-
-		</nav>
 	</div>
+	<nav id="drwMenu" class="sp">
+		<ul>
+			<li><a href="<?php echo esc_url(home_url()); ?>/#delivery">ご利用の流れ</a></li>
+			<li><a href="<?php echo esc_url(home_url()); ?>/#contract">プラン</a></li>
+			<li><a href="<?php echo esc_url(home_url()); ?>/#faq">FAQ</a></li>
+			<li class="contact"><a href="<?php echo esc_url(home_url()); ?>/#contact" class="flex"><img src="<?php echo esc_url(get_theme_file_uri()); ?>/img/ico_contact.png" width="21" height="21" alt="">お問い合わせ</a></li>
+
+		</ul>
+	</nav>
 
 </header>
