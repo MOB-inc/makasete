985643281abe0efab5b84dfd30fc744609999849
Youhei Aikou <mobinc.kakimotokikuko@gmail.com>
2022-12-28 17:05:40 +0900

    2022/12/28

--- front-page.php
+++ front-page.php
@@ -5,9 +5,10 @@
 	<section class="main-visual">
 		<div class="container-s">
 			<div class="flex">
-				<div class="flex-item1">
+				<picture class="flex-item1">
+					<source srcset="<?php echo esc_url(get_theme_file_uri()); ?>/img/new.webp" type="image/webp">
 					<img src="<?php echo esc_url(get_theme_file_uri()); ?>/img/new.png" alt="" width="88" height="88">
-				</div>
+				</picture>
 				<div class="flex-item2">
 					<p>定額デザインサービス</p>
 					<h1>MAKASETE</h1>
@@ -15,6 +16,14 @@
 			</div>
 			<p>デザインのことなら<br>すべておまかせください</p>
 			<a>資料請求・お問合わせはこちらから</a>
+		</div>
+	</section>
+	<section class="about-makasete">
+		<div class="container-s">
+			<h2><span>MAKASETE</span>ってなに？</h2>
+			<p>スタート時に必要なものを<br>定額でプロにおまかせしてみませんか？</p>
+			<p>名刺、ロゴ、HPなど、とりあえず作ったものでそろそろ変えたい・・・<br>自社にデザイナー、コーダーがいない・・・<br>そんなときに、MAKASETEに丸投げしてみませんか？
+			</p>
 		</div>
 	</section>
 </main>
