ed042f2fbb7cb029a7a908b5949312caad015d3e
Youhei Aikou <mobinc.kakimotokikuko@gmail.com>
2022-12-29 17:04:56 +0900

    2022/12/29

--- front-page.php
+++ front-page.php
@@ -2,13 +2,12 @@
 <?php get_template_part('navi-header'); ?>
 <!-- TOPページ -->
 <main id="front">
-	<section class="main-visual">
+	<section class="main-visual shapedividers_com-7008">
 		<div class="container-s">
 			<div class="flex">
-				<picture class="flex-item1">
-					<source srcset="<?php echo esc_url(get_theme_file_uri()); ?>/img/new.webp" type="image/webp">
+				<div class="flex-item1">
 					<img src="<?php echo esc_url(get_theme_file_uri()); ?>/img/new.png" alt="" width="88" height="88">
-				</picture>
+				</div>
 				<div class="flex-item2">
 					<p>定額デザインサービス</p>
 					<h1>MAKASETE</h1>
@@ -18,12 +17,64 @@
 			<a>資料請求・お問合わせはこちらから</a>
 		</div>
 	</section>
-	<section class="about-makasete">
+	<section class="about-makasete shapedividers_com-1281">
 		<div class="container-s">
 			<h2><span>MAKASETE</span>ってなに？</h2>
 			<p>スタート時に必要なものを<br>定額でプロにおまかせしてみませんか？</p>
-			<p>名刺、ロゴ、HPなど、とりあえず作ったものでそろそろ変えたい・・・<br>自社にデザイナー、コーダーがいない・・・<br>そんなときに、MAKASETEに丸投げしてみませんか？
+			<p>名刺、ロゴ、HPなど、とりあえず作ったもので<br>そろそろ変えたい・・・<br>自社にデザイナー、コーダーがいない・・・<br>そんなときに、MAKASETEに丸投げしてみませんか？
 			</p>
+			<picture>
+				<source srcset="<?php echo esc_url(get_theme_file_uri()); ?>/img/about-makasete.webp" type="image/webp">
+				<img src="<?php echo esc_url(get_theme_file_uri()); ?>/img/about-makasete.png" alt="" width="341" height="177">
+			</picture>
+		</div>
+	</section>
+	<section class="point shapedividers_com-1967">
+		<div class="container-s">
+			<ul>
+				<li>
+					<p>point.1</p>
+					<h2>自社の制作チーム感覚で<br>気軽に何度も利用出来ます。</h2>
+					<p>
+						月額定額サービスなので、修正するたびに都度お見積りが必要なく、スムーズに制作を進める事が可能です。
+						また、予算を気にせずに何度もでも、ご気軽にご依頼いただけます。
+					</p>
+					<picture>
+						<source srcset="<?php echo esc_url(get_theme_file_uri()); ?>/img/point1.webp" type="image/webp">
+						<img src="<?php echo esc_url(get_theme_file_uri()); ?>/img/point1.png" alt="" width="307" height="215">
+					</picture>
+				</li>
+				<li>
+					<p>point.2</p>
+					<h2>経験豊富なプロが作るから安心。<br>事業に集中出来ます。</h2>
+					<p>
+						戦略を踏まえたデザイン、内部の最適化等、お客様にあった最適解をご提案いたします。<br>
+						提案ベースで進んでいくので、余計なことは考えず、事業に集中して取り組む事が可能です。
+					</p>
+					<picture>
+						<source srcset="<?php echo esc_url(get_theme_file_uri()); ?>/img/point2.webp" type="image/webp">
+						<img src="<?php echo esc_url(get_theme_file_uri()); ?>/img/point2.png" alt="" width="307" height="215">
+					</picture>
+				</li>
+				<li>
+					<p>point.3</p>
+					<h2>ブランドイメージの統一。</h2>
+					<p>
+						ロゴ制作からHP製作、名刺などもワンストップで制作するため、ブランドイメージを統一して作成が可能です。<br>
+						フォントやあしらいなど、細かいイメージを統一をして、ブランドイメージの確立をお手伝いいたします。
+					</p>
+					<picture>
+						<source srcset="<?php echo esc_url(get_theme_file_uri()); ?>/img/point3.webp" type="image/webp">
+						<img src="<?php echo esc_url(get_theme_file_uri()); ?>/img/point3.png" alt="" width="307" height="215">
+					</picture>
+				</li>
+			</ul>
+			<a class="cta">お申込みはこちらから</a>
+		</div>
+	</section>
+	<section class="service">
+		<div class="container-s">
+			<h2>月額<span>69,800</span>円でここまで対応！</h2>
 		</div>
 	</section>
 </main>
